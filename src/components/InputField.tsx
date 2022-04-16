e18f62c9a8ce4838f6bd529df2878b681aeeebd0
bakemonostan <ehizomokhomion@gmail.com>
2022-04-16 01:52:07 -0700

    updated

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,18 +1,30 @@
 import './styles.css';
+import { useRef } from 'react';
+
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null); // useRef for removing the blur after pressing enter
+
   return (
-    <form className='input'>
+    <form
+      className='input'
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
         type='input'
         placeholder='Enter a task'
         className='input__box'
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
+        ref={inputRef}
       />
       <button className='input_submit' type='submit'>
         Go
